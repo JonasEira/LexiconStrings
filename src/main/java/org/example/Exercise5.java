d7fc0584e1edcdc1c80f6b37a0755c79dd6d0562
Jonas Eira <jonas.eira@gmail.com>
2022-06-09 13:47:33 +0200

    Finished Exercise5

---
+++ src/main/java/org/example/Exercise5.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Exercise5 {
+    public Exercise5(){
+
+        String s = "CAPS EQUALS SCREAMING";
+        System.out.println(s.toLowerCase());
+        System.out.println(s);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,5 +12,7 @@ public class Main {
         Exercise3 ex3 = new Exercise3();
 
         Exercise4 ex4 = new Exercise4();
+        Exercise5 ex5 = new Exercise5();
+
     }
 }
