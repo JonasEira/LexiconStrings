09f375711ddb5b0e9c2ca3310f26779f26782d7f
Jonas Eira <jonas.eira@gmail.com>
2022-06-09 13:39:02 +0200

    Finished exercise3

--- src/main/java/org/example/Exercise3.java
+++ src/main/java/org/example/Exercise3.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Exercise3 {
     public Exercise3(){
-
+        String s = "Even longer example sentence";
+        System.out.println("The character index for \"o\" is " + s.indexOf('o'));
     }
 }
