dd2fd700c4c5ca2d24b6f1634c0ee3f1485baca3
Jonas Eira <jonas.eira@gmail.com>
2022-06-09 14:52:15 +0200

    Finished Exercise11

---
+++ src/main/java/org/example/Exercise11.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Exercise11 {
+    public Exercise11() {
+        String s  = "ThisShouldBeConverted";
+        System.out.println();
+        char[] s_chars = new char[s.length()];
+        for(int n = 0; n < s.length(); n++){
+            s_chars[n] = s.charAt(n);
+        }
+        for(char c : s_chars){
+            System.out.print(c + " ");
+        }
+    }
+}
