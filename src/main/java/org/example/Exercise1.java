4c57308e58bb651e61db008adad68f2c20b0815a
Jonas Eira <jonas.eira@gmail.com>
2022-06-09 13:26:34 +0200

    Initial commit

---
+++ src/main/java/org/example/Exercise1.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Exercise1 {
+    private final String str;
+
+    public Exercise1(){
+        this.str = "Java";
+    }
+
+    public static void printStrLen(String s){
+        System.out.print(s.length());
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First String in exercise");
+        Exercise1 ex1 = new Exercise1();
+        ex1.printStrLen("Java");
+    }
+}
