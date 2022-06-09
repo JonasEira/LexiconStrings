30a64d56e755390165fd8479c5ee393cb80d2e82
Jonas Eira <jonas.eira@gmail.com>
2022-06-09 13:45:19 +0200

    Finished Exercise4

--- src/main/java/org/example/Exercise4.java
+++ src/main/java/org/example/Exercise4.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class Exercise4 {
+    public Exercise4(){
+        String s  = "Ok this is not as long!";
+        String sub = s.substring(s.indexOf("not"));
+        System.out.println(sub);
+    }
 }
